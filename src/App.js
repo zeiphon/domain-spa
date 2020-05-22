dfd9615d61050688a652fc8600dc854fce7c4d2f
Scott Butler <scottbutler2005@gmail.com>
2020-05-22 16:59:37 +1000

    Mak API call and render results to the screen

--- src/App.js
+++ src/App.js
@@ -2,16 +2,49 @@ import React from 'react';
 import './App.css';
 import Search from './components/search';
 import Footer from './components/footer';
+import axios from 'axios';
 
 function App() {
   const [suburb, setSuburb] = React.useState('Blackburn');
   const updateSuburb = function(evt) {
     setSuburb(evt.currentTarget.value);
+  };
+  const getKeyFromQueryString = function() {
+    return window.location.search.split('=')[1];
+  };
+  const runSearch = async function() {
+    const key = getKeyFromQueryString();
+    const url = 'https://api.domain.com.au/v1/listings/residential/_search?api_key=' + key;
+    const data = {
+      "listingType":"Sale",
+      "propertyTypes":[
+        "House",
+        "NewApartments",
+        "ApartmentUnitFlat",
+        "Townhouse"
+      ],
+      "listingAttributes": [
+        "NotUnderContract"
+      ],
+      "minBedrooms":2,
+      "minBathrooms":2,
+      "minCarspaces":1,
+      "maxPrice":650000,
+      "locations":[
+        {
+          "state":"VIC",
+          "suburb":suburb
+        }
+      ],
+      "sort": {
+        "sortKey": "DateListed",
+        "direction": "Descending"
+      }
+    };
+    const result = await axios.post(url, data);
+    document.querySelector("#searchResult").innerHTML = JSON.stringify(result, undefined, 2);
   }
-  const runSearch = function() {
-    document.querySelector("#searchResult").innerHTML = 'You searched for ' + suburb;
-  }
-
+ 
   return (
     <div className="App py-2 container-fluid">
       <h1 className="pb-2">Domain Property Search</h1>
@@ -21,7 +54,7 @@ function App() {
         </div>
         <div className="col-9 p-2 pl-4" id="output">
           <span>{suburb}</span><br />
-          <span id="searchResult"></span>
+          <pre id="searchResult"></pre>
         </div>
       </div>
       <Footer />
