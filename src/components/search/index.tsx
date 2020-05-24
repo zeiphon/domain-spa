bb64c55411e738c0c999dc65f70ec0e69def0df9
Scott Butler <scottbutler2005@gmail.com>
2020-05-24 17:05:03 +1000

    Responsive search fields

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -10,8 +10,23 @@ function Search(props: any) {
     return (
         <>
             <div id="search" className="p-2">
-                <label htmlFor="suburb">Suburb:</label>
-                <input id="suburb" type="text" defaultValue={suburb} onChange={updateSuburb} onBlur={updateSuburb} />
+                <div className="row">
+                    <div className="col-lg-4">
+                        <label htmlFor="state">State:</label>
+                    </div>
+                    <div className="col-lg-8">
+                        <input id="state" type="text" value="VIC" disabled />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <label htmlFor="suburb">Suburb here:</label>
+                    </div>
+                    <div className="col-lg-8">
+                        <input id="suburb" type="text" defaultValue={suburb} onChange={updateSuburb} onBlur={updateSuburb} />
+                    </div>
+                </div>
+
                 <input type="button" value="Search" onClick={runSearch} />
             </div>
         </>
