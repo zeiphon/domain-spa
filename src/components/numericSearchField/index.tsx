b40e778baf906b4b38eee71ae36155713cd1a149
Scott Butler <scottbutler2005@gmail.com>
2020-06-05 17:49:14 +1000

    CSS tweaks

--- src/components/numericSearchField/index.tsx
+++ src/components/numericSearchField/index.tsx
@@ -15,9 +15,9 @@ function NumericSearchField(props: any) {
         <>
             <div className="row numeric-search-field mb-2">
                 <div className="col-lg-4 mt-1">
-                    <label htmlFor={id}>{label}:</label>
+                    <label className="mb-0" htmlFor={id}>{label}:</label>
                 </div>
-                <div className="col-lg-8 align-middle controls">
+                <div className="col-lg-8 align-bottom controls mt-2">
                     <span className="down bg-light text-dark p-1 border border-secondary rounded-left d-inline-block" onClick={decrement}>-</span>
                     <span className="value px-2 py-1 border-top border-bottom border-secondary d-inline-block">{defaultValue.toLocaleString()}</span>
                     <span className="up bg-light text-dark p-1 border border-secondary rounded-right d-inline-block" onClick={increment}>+</span>
