f47b9f5f92f9a8b057ab52d9bb70304c0756c04a
Scott Butler <scottbutler2005@gmail.com>
2020-06-14 09:13:02 +1000

    Add icons to search fields

--- src/components/numericSearchField/index.tsx
+++ src/components/numericSearchField/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NumericSearchField(props: any) {
-    const { label, id, defaultValue, updateHandler, interval } = props;
+    const { label, id, defaultValue, updateHandler, interval, icon } = props;
 
     const decrement = function() {
         if (defaultValue - interval >= 0) {
@@ -17,7 +17,7 @@ function NumericSearchField(props: any) {
         <>
             <div className="row numeric-search-field mb-2">
                 <div className="col-lg-4 mt-2">
-                    <label className="mb-0" htmlFor={id}>{label}:</label>
+                    <label className="mb-0" htmlFor={id}>{label}<i className={icon} />:</label>
                 </div>
                 <div className="col-lg-8 align-bottom controls mt-2">
                     <button className="down bg-light text-dark p-1 border border-secondary rounded-left d-inline-block" onClick={decrement}>-</button>
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -33,11 +33,11 @@ function Search(props: any) {
                         <input id="suburb" type="text" placeholder="e.g Kew, Rye" defaultValue={suburbs} onChange={updateSuburbs} onBlur={updateSuburbs} />
                     </div>
                 </div>
-                <NumericSearchField label="Min. bedrooms" id="minBeds" defaultValue={minBeds} updateHandler={updateMinBeds} interval={1} />
-                <NumericSearchField label="Min. bathrooms" id="minBaths" defaultValue={minBaths} updateHandler={updateMinBaths} interval={1} />
-                <NumericSearchField label="Min. car spaces" id="minCarSpaces" defaultValue={minCarSpaces} updateHandler={updateMinCarSpaces} interval={1} />
-                <NumericSearchField label="Max. price" id="maxPrice" defaultValue={maxPrice} updateHandler={updateMaxPrice} interval={10000} />
-                <NumericSearchField label="Max. distance from train" id="maxDistanceFromTrain" defaultValue={maxDistanceFromTrain} updateHandler={updateMaxDistanceFromTrain} interval={0.25} />
+                <NumericSearchField label="Min. " icon="icon-bed" id="minBeds" defaultValue={minBeds} updateHandler={updateMinBeds} interval={1} />
+                <NumericSearchField label="Min. " icon="icon-bath" id="minBaths" defaultValue={minBaths} updateHandler={updateMinBaths} interval={1} />
+                <NumericSearchField label="Min. " icon="icon-cab" id="minCarSpaces" defaultValue={minCarSpaces} updateHandler={updateMinCarSpaces} interval={1} />
+                <NumericSearchField label="Max. " icon="icon-dollar" id="maxPrice" defaultValue={maxPrice} updateHandler={updateMaxPrice} interval={10000} />
+                <NumericSearchField label="Max. km from " icon="icon-train" id="maxDistanceFromTrain" defaultValue={maxDistanceFromTrain} updateHandler={updateMaxDistanceFromTrain} interval={0.25} />
 
                 <div className="row">
                     <div className="offset-lg-4 col-lg-8">
