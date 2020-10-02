463d1ef8ee41c91c7e772708fb5c387693a095c4
Scott Butler <scottbutler2005@gmail.com>
2020-10-02 12:06:07 +1000

    Add agent banner and colour conversion functions

--- src/components/searchResult/index.tsx
+++ src/components/searchResult/index.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import NewTabLink from '../newTabLink';
 import SimpleCarousel from '../simpleCarousel';
 
+function hexToRgb(hex: string): number[] {
+    const withoutHash = hex.replaceAll('#', '');
+    const parts = withoutHash.length === 3
+        ? [ withoutHash[0], withoutHash[1], withoutHash[2] ]
+        : [ withoutHash.substr(0, 2), withoutHash.substr(2, 2), withoutHash.substr(4, 2) ];
+    return parts.map((x) => parseInt(`0x${x}`, 16));
+}
+
+// https://stackoverflow.com/a/3943023
+function calculateTextColourForBackgroundColour(rgb: number[]): string {
+    return (rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 186
+        ? "#000000"
+        : "#ffffff";
+}
+
 function SearchResult(props: any) {
     const {
         closestStops, data
@@ -12,6 +27,8 @@ function SearchResult(props: any) {
         ? `${data.listing.propertyDetails.unitNumber}/${data.listing.propertyDetails.streetNumber} ${data.listing.propertyDetails.street}`
         : `${data.listing.propertyDetails.streetNumber} ${data.listing.propertyDetails.street}`;
     const imageAltText = `Image for ${streetAddress}`;
+    const agentBackgroundColour = data.listing.advertiser.preferredColourHex;
+    const agentTextColour = calculateTextColourForBackgroundColour(hexToRgb(agentBackgroundColour));
 
     //Select at most the first two closest stops, then add markup for rendering
     const closestStopsMarkup = closestStops
@@ -34,6 +51,9 @@ function SearchResult(props: any) {
                     <div className="border border-secondary h-100 shadow d-flex flex-column ">
                         <div className="px-0 text-center overflow-hidden carousel">
                             <SimpleCarousel key={data.listingSlug} urls={imageUrls} altText={imageAltText} />
+                        </div>
+                        <div className="agent-banner pl-3" style={{backgroundColor: agentBackgroundColour, color: agentTextColour}}>
+                            {data.listing.advertiser.name}
                         </div>
                         <div className="px-3 pt-1 pb-2 mb-auto">
                             <span className="d-block font-weight-bold text-truncate">{data.listing.priceDetails.displayPrice}</span>
